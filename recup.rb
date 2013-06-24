355cbd0ab1d75fde8d33a634eb7b24ea64dd2b34
Thomas Salmon <th_s@hotmail.fr>
2013-06-24 19:30:03 +0200

    on peut maintenant acceder a toute les pages du forum

--- recup.rb
+++ recup.rb
@@ -1,11 +1,13 @@
 require 'open-uri'
 require 'rexml/document'
 
+LOG = ["app_and_gnw", "FC?4554?"]
 SITE =  "http://ws.jeuxvideo.com/forums/"
-
+NEXT_PAGE = /0-.*<\/page_suivante>/
 def page (show) 
-  reception = open(SITE + show, :http_basic_authentication => ["app_and_gnw", "FC?4554?"]).read()
-  puts reception 
+  reception = open(SITE + show, :http_basic_authentication => LOG).read()
+  suivant = (reception.match NEXT_PAGE).to_s.sub /<\/page_suivante>/, ""
+  page suivant
 end
 
 page("0-19163-0-1-0-1-0-0.xml")
