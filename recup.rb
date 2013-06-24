cdacaa94ada09328f399952f8ef514fc2e649a25
Thomas Salmon <th_s@hotmail.fr>
2013-06-24 19:07:34 +0200

    fonction

--- recup.rb
+++ recup.rb
@@ -1,13 +1,11 @@
 require 'open-uri'
-lien = "http://ws.jeuxvideo.com/forums/"
-page = lien + "0-19163-0-1-0-1-0-0.xml"
-topico = open(page, :http_basic_authentication => ["appandr", "e32!cdf"]).read().split(/<topic>/)[0]
+require 'rexml/document'
 
-page_suivante = topico.split("\n")[5].slice(/[0-9][^<]+/)
-puts page_suivante
-1.upto(1000) do |x|
-	topico = open(lien + page_suivante, :http_basic_authentication => ["appandr", "e32!cdf"]).read().split(/<topic>/)[0]
+SITE =  "http://ws.jeuxvideo.com/forums/"
 
-	page_suivante = topico.split("\n")[5].slice(/[0-9][^<]+/)
-	puts page_suivante
+def page (show) 
+  reception = open(SITE + show, :http_basic_authentication => ["app_and_gnw", "FC?4554?"]).read()
+  puts reception 
 end
+
+page("0-19163-0-1-0-1-0-0.xml")
