635e84d9f2fddaa876fe5d220fd919669807f7dc
Thomas Salmon <th_s@hotmail.fr>
2013-06-27 17:08:50 +0200

    optimisation

--- recup.rb
+++ recup.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'rexml/document'
 
 LOG = ["app_and_gnw", "FC?4554?"]
+CO = :http_basic_authentication
 SITE =  "http://ws.jeuxvideo.com/"
 PSEUDO = {}
 RGX_NEXT_PAGE = /0-.*<\/page_suivante>/
@@ -11,10 +12,6 @@ RGX_SUIV = /.*<\/suiv_rapide>/
 RGX_DERNIERE = /.*<\/derniere_page>/
 RGX_CONTENT = "<contenu>.*"
 RGX_PSEUDO = /jv:\/\/profil\/[\w_\[\]\(\)\{\}-]*\.xml/
-
-def rgx_suppr(x, y)
-  return x.to_s.sub y, ""
-end
 
 #lecture des réponses d'une page de sujet
 def pseudo(show)
@@ -25,65 +22,59 @@ def pseudo(show)
   end
 end
 
+def topic_suivant(suiv, last)
+  if(suiv != "<suiv_rapide><\/suiv_rapide>") then
+    return topic(suiv[25..-15])
+  end
+  return (last != "<derniere_page></derniere_page>") ? topic(last[27..-17]) : nil
+end
+
 #lecture page par page d'un sujet
 def topic(show)
-  reception = open(SITE + "forums/" + show, :http_basic_authentication => LOG).read()
-  suiv = (reception.match RGX_SUIV).to_s
+  reception = open(SITE + "forums/" + show, CO => LOG).read()
+  suivant = (reception.match RGX_SUIV).to_s
+  dernier = (reception.match RGX_DERNIERE).to_s
   reception.scan(RGX_PSEUDO).each {|p| pseudo(p[12..-1])}
-  if(suiv == "<suiv_rapide><\/suiv_rapide>") then
-    dernier = (reception.match RGX_DERNIERE).to_s
-    if(dernier != "<derniere_page><\/derniere_page>") then # nb pages = 2
-      topic(dernier[27..-17])
-    end
-  else # nb pages > 2
-    topic(suiv[25..-15])
-  end
+  topic_suivant(suivant, dernier)
 end
 
 #lecture page par page de la liste des sujets
 def page (show)
-  if(show == "") then
-    return
-  else
-    reception = open(SITE + "forums/" +  show, :http_basic_authentication => LOG).read()
-    suivant = (reception.match RGX_NEXT_PAGE).to_s[0..-17]
-    topics = reception.scan(RGX_TOPIC).each {|link| topic(link[0..-14])}
-    page(suivant)
+  if(show != "") then
+    reception = open(SITE + "forums/" + show, CO => LOG).read()
+    reception.scan(RGX_TOPIC).each {|link| topic(link[0..-14])}
+    page((reception.match RGX_NEXT_PAGE).to_s[0..-17])
   end
 end
 
 def profil(show)
   s =  (show.sub /\]/, "%5D").to_s.sub /\[/, "%5B"
-  begin
-    reception = open(SITE + "profil/" + s, :http_basic_authentication => LOG).read()
-    age = (reception.match /.*<\/age>/).to_s
-    PSEUDO[show][1] = (age.match /[0-9]+/).to_s
-  rescue URI::InvalidURIError
-    puts "\""+show + "\" n'a pas pu etre identifié" 
-  end
+  reception = open(SITE + "profil/" + s, CO => LOG).read()
+  age = (reception.match /.*<\/age>/).to_s
+  PSEUDO[show][1] = (age.match /[0-9]+/).to_s
 end
 
 def forum_jeu()
-  nom = ""
+  nom = SITE + "search_forums_sug/"
   ARGV.each {|arg| nom+="-"+arg}
-  reception = open(SITE + "search_forums_sug/" + nom[1..-1], :http_basic_authentication => LOG).read()
+  reception = open(nom, CO => LOG).read()
   page("0-"+(reception.match /[0-9]+<\/id>/).to_s[0..-6]+"-0-1-0-1-0-0.xml")
 end
 
+# algo premiere partie: 
+#   on va sur le forum
+#   pour toutes les 25 topics de forums:
+#      ouvrir topic
+#      pour chaque page du topic:
+#          recuperer infos
+
+# algo deuxieme partie:
+#   pour chaque profil:
+#      recuperer age
 if __FILE__ == $0
   MyFile = File.open("result", "w")
-  # algo premiere partie: 
-  #   on va sur le forum
-  #   pour toutes les 25 topics de forums:
-  #      ouvrir topic
-  #      pour chaque page du topic:
-  #          recuperer infos
-  forum_jeu()
-  # algo deuxieme partie:
-  #   pour chaque profil:
-  #      recuperer age
-  PSEUDO.each{ |key, value| profil(key)}
+  forum_jeu() # part. 1
+  PSEUDO.each{ |key, value| profil(key)} # part. 2
   PSEUDO.each {|key, value| MyFile.write(key + " " + value[1].to_s + " " + value[0].to_s + "\n") }
   MyFile.close
-  end
-                 
+end
